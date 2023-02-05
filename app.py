b790b1a11fdc633bd3276711a127839237f66930
kalurko <kalurko@gmail.com>
2023-02-05 14:09:55 +0300

    not working do no know how to fix

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from main.views import main_blueprint
-# from api.views import api_blueprint
+from api.views import api_blueprint
 
 app = Flask(__name__)
-# app.config['JSON_AS_ASCII'] = False
+app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(main_blueprint)
-# app.register_blueprint(api_blueprint)
+app.register_blueprint(api_blueprint)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return 'Страница не найдена!'
+
+
+@app.errorhandler(500)
+def not_found_error(error):
+    return 'СЛшибка на сервере'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
